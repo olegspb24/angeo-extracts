a94ed3fdae824edb507fe7dd4a1d2437980a371b
Oleg <olegspb24@gmail.com>
2026-01-05 17:52:29 +0300

    remove redundant 'use client'

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-'use client';
-
-import Header from './components/Header';
+﻿import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Products from './components/Products';
